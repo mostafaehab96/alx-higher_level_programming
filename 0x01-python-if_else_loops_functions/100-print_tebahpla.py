b3392ef1e52c788d5082fb1043111a7aa79f4eb7
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-07 15:54:47 +0000

    Add string format in task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for c in range(ord('z'), ord('a') - 1, -1):
-    print(chr(c) if c % 2 == 0 else chr(c).upper(), end="")
+    print("{}".format(chr(c) if c % 2 == 0 else chr(c).upper()), end="")
