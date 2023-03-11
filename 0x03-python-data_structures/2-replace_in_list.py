81812429b27f88b14171b5e073154d6825bd6666
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-11 12:52:41 +0000

    Add task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+    if not idx < 0 or not idx >= length:
+        my_list[idx] = element
+    return my_list
