17807e2a39bc9d6d4afbc0b421727928fb018e1c
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-11 13:21:55 +0000

    Fix logic in task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if my_list is not None:
-        length = len(my_list)
-        if not idx < 0 or not idx >= length:
-            my_list[idx] = element
+    length = len(my_list)
+    if not idx < 0 and not idx >= length:
+        my_list[idx] = element
     return my_list
