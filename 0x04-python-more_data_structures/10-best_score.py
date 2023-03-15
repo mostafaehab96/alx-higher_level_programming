f9892835cf1d2b6ea716272ac7a2ae324876fa37
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-15 15:36:06 +0000

    Fix error name of the dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,12 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
-    return [k for k, v in dictionary.items() if v == max_value][0]
+    return [k for k, v in a_dictionary.items() if v == max_value][0]
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
