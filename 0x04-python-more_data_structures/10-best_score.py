65c613a3438f10afd98d5b3961236e7bb4283d63
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-15 16:04:52 +0000

    Add task 10 new approach

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@
 
 def best_score(a_dictionary):
     try:
-        max_value = max(a_dictionary.values())
-    except (TypeError, AttributeError):
+        return max(a_dictionary, key=a_dictionary.get)
+    except (TypeError, AttributeError, ValueError):
         return None
-    return [k for k, v in a_dictionary.items() if v == max_value][0]
