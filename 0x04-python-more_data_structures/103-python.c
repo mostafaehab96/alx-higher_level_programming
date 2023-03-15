5f6cb2cb5f4b7dd05c4d76d056a621973c45b635
mostafaehab <mostafa96ehab@gmail.com>
2023-03-15 21:52:29 +0000

    Add task 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,27 @@
 #include <Python.h>
 
+void print_python_bytes(PyObject *p);
+
+void print_python_list(PyObject *p)
+{
+	int size = (int)PyList_Size(p);
+	int i;
+	PyTypeObject *type;
+	PyObject *item;
+	int allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %i\n", (int) size);
+	printf("[*] Allocated = %i\n", allocated);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		type = Py_TYPE(item);
+		printf("Element %i: %s\n", i, type->tp_name);
+		if (strcmp(type->tp_name, "bytes") == 0)
+			print_python_bytes(item);
+	}
+}
+
 void print_python_bytes(PyObject *p)
 {
 	int size;
