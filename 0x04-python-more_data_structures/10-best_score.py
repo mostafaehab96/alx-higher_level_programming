ee44c2a46097500052475711ae67faf850c6e9bc
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-15 15:36:58 +0000

    Remove test code!

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,11 +6,3 @@ def best_score(a_dictionary):
         return None
     max_value = max(a_dictionary.values())
     return [k for k, v in a_dictionary.items() if v == max_value][0]
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
