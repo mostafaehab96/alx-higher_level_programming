0be03364e78a07971d6000220480a3563427b3c8
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-15 15:39:38 +0000

    Check if score is None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
+    if max_value is None:
+        return None
     return [k for k, v in a_dictionary.items() if v == max_value][0]
