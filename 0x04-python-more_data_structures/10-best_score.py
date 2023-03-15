85730af11a9dc405d0d05401865b31ee7a980aa3
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-15 15:29:37 +0000

    Add task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max_value = max(a_dictionary.values())
+    return [k for k, v in dictionary.items() if v == max_value][0]
