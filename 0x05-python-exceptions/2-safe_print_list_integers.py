773e2a350ff9c39f8b160bb4680ffce3aee13766
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-27 13:32:36 +0000

    Remove IndexError handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (TypeError, IndexError, ValueError):
+        except (TypeError, ValueError):
             pass
     print()
     return count
