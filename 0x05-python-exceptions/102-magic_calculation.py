a994e5cb4813a65f1dfb74efb9a62bdcb1176902
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-27 22:48:24 +0000

    Add task 102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from dis import dis
+
+def magic_calculation(a, b):
+    result = 1
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += a ** b / i
+        except:
+            result = a + b
+            break
+        finally:
+            continue
+    return result
