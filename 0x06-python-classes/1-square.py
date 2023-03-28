0c79f2d411dc8912c07fce434d8cc338e84d0d7c
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-28 19:40:54 +0000

    Add task 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module define a simple class Square."""
+
+
+class Square:
+    """A simple class Square.
+    Attributes:
+        size (int): the size of the square
+    """
+    def __init__(self, size):
+        """The init method initializes the size of the square."""
+        self.__size = size
