47bb44873039b57a32c5970102044151357ae223
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-28 21:17:39 +0000

    Add task 6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""This module define a simple class Square."""
+
+
+class Square:
+    """A simple class Square.
+    Attributes:
+        size (int): the size of the square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """The init method initializes the size of the square."""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Calculate the area of the square.
+        Returns:
+            int: the area
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for private size."""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter for private size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """Getter for private position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple:
+            if len(list(filter(lambda x: type(x) is int, value))) == 2:
+                self.__position = value
+                return
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        """prints the square on the screen using # with postion of (x, y)."""
+        print("\n" * self.__position[1], end="")
+        line = " " * self.__position[0] + "#" * self.__size + "\n"
+        print(line * self.__size, end="" if self.__size > 0 else "\n")
