21da36152fcffc8c3444f6aad62a05cc16166aa8
mostafaehab96 <mostafa96ehab@gmail.com>
2023-03-28 19:45:12 +0000

    Add task 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module define a simple class Square."""
+
+
+class Square:
+    """A simple class Square.
+    Attributes:
+        size (int): the size of the square
+    """
+    def __init__(self, size=0):
+        """The init method initializes the size of the square."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
