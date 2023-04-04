3f26f05404485d198604b15422207a6ffc6b5faa
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-04 00:42:27 +0000

    Add task 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines say_my_name_function."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Simple function for saying a name.
+    format: My name is <first name> <last name>
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
