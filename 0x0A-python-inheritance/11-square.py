ec7dacde6532d64b9f79de87b0c546e6786e0a48
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-11 14:37:48 +0000

    Add task 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class empty."""
+
+    def area(self):
+        """Basic area function."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value is integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Simple Rectangle class."""
+
+    def __init__(self, width, height):
+        """intialized with width and height must be int."""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Calculates the area of rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the description of rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """Simple square class."""
+
+    def __init__(self, size):
+        """Intialize square with size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculated the area of square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return description of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
