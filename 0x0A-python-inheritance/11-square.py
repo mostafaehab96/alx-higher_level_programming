ff7835f8b644efd28ae456b3c4e8492064e30f9e
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-13 12:17:23 +0000

    Fix 10, 11 tasks

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,25 +2,7 @@
 """Defines a Square class."""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Simple Rectangle class."""
-
-    def __init__(self, width, height):
-        """intialized with width and height must be int."""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Calculates the area of rectangle."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns the description of rectangle."""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
