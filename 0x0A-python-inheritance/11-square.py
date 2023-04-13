39b3f2a387b18284907bf9a0f28e8d384de5a983
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-13 12:14:30 +0000

    Fix errors in tasks 8, 10, 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 """Defines a Square class."""
 
-
-class BaseGeometry:
-    """BaseGeometry class empty."""
-
-    def area(self):
-        """Basic area function."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value is integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value < 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
