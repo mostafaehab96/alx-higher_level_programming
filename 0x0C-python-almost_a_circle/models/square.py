68372f44fb4bf1c0db29396b16870e92a08caa7d
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-16 23:15:39 +0000

    Add tasks 10, 11 and 12, square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""Defines a Square class the inherits from Rectangle class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A simple Square class inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the Square instance by using the
+        Rectangle class initializer.
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter for the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size."""
+        self.width = value
+        self.height = value
+        self.__size = value
+
+    def __str__(self):
+        """Overriding the str method in Rectangle class
+        which returns the str representation of Square.
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Updates the Square instance with args or kwargs."""
+        if len(args) == 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                if k == 'size':
+                    self.size = v
+                if k == 'x':
+                    self.x = v
+                if k == 'y':
+                    self.y = v
+            return
+        attrs = ['id', 'size', 'x', 'y']
+        for i in range(len(args)):
+            k = attrs[i]
+            v = args[i]
+            if k == 'id':
+                self.id = v
+            if k == 'size':
+                self.size = v
+            if k == 'x':
+                self.x = v
+            if k == 'y':
+                self.y = v
