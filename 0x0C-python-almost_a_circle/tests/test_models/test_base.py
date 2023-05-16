c80b463362df0a2ae612eeedcd86ed65a027ae9b
mostafaehab96 <mostafa96ehab@gmail.com>
2023-05-16 21:21:33 +0000

    Adding all files changed in previous projects.

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,10 +3,15 @@
 """Unit testing for testing Base class."""
 import unittest
 from models.base import Base
-
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
     """A class for testing Base class."""
+
+    def setUp(self):
+        """For testing the id."""
+        Base._Base__nb_objects = 0
 
     def test_id(self):
         """Testing id of the Base class."""
@@ -14,3 +19,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(Base().id, 2)
         self.assertEqual(Base(5).id, 5)
         self.assertEqual(Base().id, 3)
+
+    def test_to_json_string(self):
+        """Testing the json string representation."""
+        pass
