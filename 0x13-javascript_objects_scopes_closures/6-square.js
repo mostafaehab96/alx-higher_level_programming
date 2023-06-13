b49252d4c354fe6f17cb966271d012a6f687a96d
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-13 22:13:35 +0000

    Changed the solution of task 6-square.js to inherit from Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const SquareP = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareP {
   charPrint (c) {
     if (c) {
       let line = '';
