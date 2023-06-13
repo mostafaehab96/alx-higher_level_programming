8a8b4d2fe05ec5aa3ca4669f82cd1be8da4ef5b1
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-13 22:09:40 +0000

    Add task 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      let line = '';
+      for (let i = 0; i < this.height; i++) {
+        line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += c;
+        }
+        console.log(line);
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
