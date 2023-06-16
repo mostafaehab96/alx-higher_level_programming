0c306dd1902333aa06709050eca2d09787e5fcae
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-16 23:14:12 +0000

    Add task 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" a script that lists all states with a name starting
+    with N (upper N) from the database.
+    Given the username, password, the database name
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    args = sys.argv
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    search_state = args[4]
+    # Setting the connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
+    # Perparing cursor object
+    cursor = db.cursor()
+    # Execute the sql
+    sql = """
+    SElECT name
+    FROM cities
+    WHERE state_id = (
+        SELECT id FROM states
+        WHERE name LIKE BINARY %s)
+    ORDER BY cities.id;
+    """
+    cursor.execute(sql, (search_state,))
+    # Readingt the data
+    data = cursor.fetchall()
+    i = 0
+    for row in data:
+        i += 1
+        print(f"{row[0]}", end=", " if i < len(data) else "\n")
