88d883293b4453934184f00afddb9b73038db985
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-16 18:23:09 +0000

    Add project 0x0F-python-object_relational_mapping, README and first task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+	args = sys.argv
+	username = args[1]
+	password = args[2]
+	db_name = args[3]
+	#Setting the connection
+	db = MySQLdb.connect(host="localhost", port=3306, user=username,
+		passwd=password, db=db_name, charset="utf8")
+	#Perparing cursor object
+	cursor = db.cursor()
+	#Execute the sql
+	cursor.execute("SELECT * FROM states ORDER BY id")
+	#Readingt the data
+	data = cursor.fetchall()
+	for row in data:
+		print(f"({row[0]}, '{row[1]}')")
+
