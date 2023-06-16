c4005e0d8e7ccfa438a0062258627e88ac710d4f
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-16 20:15:46 +0000

    Fix pycodestyle for 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,18 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-	args = sys.argv
-	username = args[1]
-	password = args[2]
-	db_name = args[3]
-	#Setting the connection
-	db = MySQLdb.connect(host="localhost", port=3306, user=username,
-		passwd=password, db=db_name, charset="utf8")
-	#Perparing cursor object
-	cursor = db.cursor()
-	#Execute the sql
-	cursor.execute("SELECT * FROM states ORDER BY id")
-	#Readingt the data
-	data = cursor.fetchall()
-	for row in data:
-		print(f"({row[0]}, '{row[1]}')")
-
+    args = sys.argv
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    # Setting the connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
+    # Perparing cursor object
+    cursor = db.cursor()
+    # Execute the sql
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    # Readingt the data
+    data = cursor.fetchall()
+    for row in data:
+        print(f"({row[0]}, '{row[1]}')")
