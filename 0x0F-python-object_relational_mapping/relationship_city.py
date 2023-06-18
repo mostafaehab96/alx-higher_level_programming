7b0bad008fb866c7df2c19b5d4661222bf5e562a
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-18 23:29:43 +0000

    Add state relationship in City and add back_populates in cities relationship in State

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,7 +3,7 @@
 """ Script defines City class that inherits from Base. """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from relationship_state import Base, State
 
 
@@ -14,3 +14,4 @@ class City(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,4 +14,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete")
+    cities = relationship("City", back_populates="state", cascade="all, delete")
