e5c4de443b33f01b296ea7cb870b3543a3589bb1
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-04 21:14:12 +0000

    Add task 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Gets the header value of url passed as argument."""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request
+
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        headers = response.info()
+        print(headers.get("X-Request-Id", ""))
