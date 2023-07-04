fa8047f6cd02d173a6b9cec6eae56cb5e32eb21c
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-04 21:41:53 +0000

    Add task 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Sends request to url passed and checks for errors."""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request
+    from urllib.error import HTTPError
+
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            data = response.read()
+            print(data.decode('utf-8'))
+    except HTTPError as e:
+        print(f"Error code: {e.code}")
