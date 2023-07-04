b278a708854d4180c9def47c87c10a12a6de3c02
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-04 21:07:56 +0000

    Add space after -

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         data = response.read()
         print("Body response:")
-        print(f"\t-type: {type(data)}")
-        print(f"\t-content: {data}")
-        print(f"\t-utf8 content: {data.decode('utf-8')}")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode('utf-8')}")
