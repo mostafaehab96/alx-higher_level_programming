913cfba8d937f59461dbe3bb0ec01cb74dde3331
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-05 01:55:27 +0000

    Add task  8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = "http://0.0.0.0:5000/search_user"
+    params = {"q": q}
+
+    response = requests.post(url, data=params)
+    try:
+        data = response.json()
+        if len(data) == 0:
+            print("No result")
+        else:
+            print(f"[{data.get('id')}] {data.get('name')}")
+    except JSONDecodeError:
+        if response.status_code == 204:
+            print("No result")
+        else:
+            print("Not a valid JSON")
