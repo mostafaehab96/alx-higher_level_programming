fbd4f0fd88820c919ad3053269f54581f02a4ae3
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-05 03:32:07 +0000

    Remove personal token

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,12 +10,10 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
 
-    headers = {"Authorization":
-               f"Bearer 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"}
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     commits = response.json()
     for commit in commits[0: 10]:
-        print(f"{commit.get('sha')}:
-                {commit.get('commit').get('author').get('name')}")
+        print(f"{commit.get('sha')}: "
+              f"{commit.get('commit').get('author').get('name')}")
