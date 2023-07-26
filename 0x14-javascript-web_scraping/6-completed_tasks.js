a58b1bfea17b7c896178f8dad7d7b8b3107fb7bf
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-26 00:16:34 +0000

    Add default url

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const url = args[2];
+const url = args[2] ? args[2] : 'https://jsonplaceholder.typicode.com/todos';
 const request = require('request');
 
-if (url) {
-  request(url, (err, response, body) => {
-    if (err) {
-      console.log(err);
-    }
-    if (response) {
-      const todos = JSON.parse(body);
-      const completed = {};
-      for (const todo of todos) {
-        if (todo.completed) {
-          completed[todo.userId] = completed[todo.userId] ? ++completed[todo.userId] : 1;
-        }
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  if (response) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (const todo of todos) {
+      if (todo.completed) {
+        completed[todo.userId] = completed[todo.userId] ? ++completed[todo.userId] : 1;
       }
-      console.log(completed);
     }
-  });
-}
+    console.log(completed);
+  }
+});
